7270e269c531848df6bfff1f1e33e946291825a2
dingjianzhong <80936735@yonghui.cn>
2022-03-11 15:23:06 +0800

    🎸 feat(init): 初始化

---
+++ rime.lua
@@ -0,0 +1,7 @@
+-- date_translator: 将 `date` 翻译为当前日期
+-- 详见 `lua/date.lua`:
+date_translator = require("date")
+
+-- time_translator: 将 `time` 翻译为当前时间
+-- 详见 `lua/time.lua`
+time_translator = require("time")
